87f79f8f18517f8267e23b5bb6fc1bc9240ce8e5
Nicolas Martinez <nmartinez@corpit-consulting.com>
2020-07-06 16:27:53 -0300

    fix output

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "bucket_name" {
-  value = aws_s3_bucket.bucket_logs.name
+  value = aws_s3_bucket.bucket_logs.bucket
 }
 
 output "bucket_prefix" {
-  value = aws_s3_bucket.bucket_logs.prefix
+  value = aws_s3_bucket.bucket_logs.bucket_prefix
 }
